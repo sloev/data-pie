b3ad24386932ba7924935734299958e44e19f711
sloev <johannesgj@gmail.com>
2013-10-21 00:21:22 +0200

    test

--- data-pie/network/Bonjour.py
+++ data-pie/network/Bonjour.py
@@ -3,33 +3,32 @@ Created on Oct 20, 2013
 
 @author: johannes
 '''
+
+import time
 import select
 import sys
-import time
 import pybonjour
 
 class Bonjour():
     def __init__(self,name,regtype,port):
-        print()
-        self.sdRef = pybonjour.DNSServiceRegister(name,
-                                     regtype,
-                                     port,
-                                     self.register_callback)
+        def register_callback(sdRef, flags, errorCode, name, regtype, domain):
+            if errorCode == pybonjour.kDNSServiceErr_NoError:
+                print 'Registered service:'
+                print '  name    =', name
+                print '  regtype =', regtype
+                print '  domain  =', domain
+                
+        self.sdRef = pybonjour.DNSServiceRegister(name = name,
+                                     regtype = regtype,
+                                     port = port,
+                                     callBack = register_callback)
         ready = select.select([self.sdRef], [], [])
-
         if self.sdRef in ready[0]:
             pybonjour.DNSServiceProcessResult(self.sdRef)
 
-    def register_callback(self,sdRef, flags, errorCode, name, regtype, domain):
-        if errorCode == pybonjour.kDNSServiceErr_NoError:
-            print 'Registered service:'
-            print '  name    =', name
-            print '  regtype =', regtype
-            print '  domain  =', domain
-            
     def close(self):
         self.sdRef.close()
-        
+   
 def main():
     bonjour=Bonjour("TestService", '_test._tcp', 1234) 
     time.sleep(20)
@@ -37,4 +36,3 @@ def main():
     
 if __name__ == '__main__':
     main()
-
