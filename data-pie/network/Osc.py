a5ac6a5665eeb22ea157cb510d52df35e71e2169
sloev <johannesgj@gmail.com>
2013-10-25 00:16:37 +0200

    lol

--- data-pie/network/Osc.py
+++ data-pie/network/Osc.py
@@ -81,9 +81,12 @@ class bonjourThread(threading.Thread):
     def run(self):
         while not self.finished.isSet():
             print("running")
-            ready = select.select([self.sdRef], [], [])
-            if self.sdRef in ready[0]:
-                pybonjour.DNSServiceProcessResult(self.sdRef)
+            try:
+                ready = select.select([self.sdRef], [], [])
+                if self.sdRef in ready[0]:
+                    pybonjour.DNSServiceProcessResult(self.sdRef)
+            except ValueError:
+                print("eerroorr")
         print("end")
 #         print "a"
 #         print "b"
