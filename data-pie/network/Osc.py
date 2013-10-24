4946d0306df6fbe97d410c69ff3749c4e41d9237
sloev <johannesgj@gmail.com>
2013-10-24 23:49:06 +0200

    test

--- data-pie/network/Osc.py
+++ data-pie/network/Osc.py
@@ -79,22 +79,17 @@ class bonjourThread(threading.Thread):
                                                   callBack = register_callback)        
     def run(self):
         while not self.finished.isSet():
-            time.sleep(1)
-#             try:
-#                 ready = select.select([self.sdRef], [], [])
-#                 if self.sdRef in ready[0]:
-#                     pybonjour.DNSServiceProcessResult(self.sdRef)
-#             except self.finished.isSet():
-#                 print "a"
-#                 self.sdRef.close()
-#                 print "b"
-#                 break
-
+            ready = select.select([self.sdRef], [], [])
+            if self.sdRef in ready[0]:
+                pybonjour.DNSServiceProcessResult(self.sdRef)
+        print "a"
+        self.sdRef.close()
+        print "b"
+        self.join()
 
     def stop (self):
         self.finished.set()
         print("lol")
-        self.join()
 
         
 def main():
