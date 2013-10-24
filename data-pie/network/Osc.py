779ac520f6428c0fd855e572fc4d210fd16e60d4
sloev <johannesgj@gmail.com>
2013-10-25 00:32:11 +0200

    test

--- data-pie/network/Osc.py
+++ data-pie/network/Osc.py
@@ -84,9 +84,9 @@ class bonjourThread(threading.Thread):
                 ready = select.select([self.sdRef], [], [])
                 if self.sdRef in ready[0]:
                     pybonjour.DNSServiceProcessResult(self.sdRef)
-            except ValueError as e:
+            except:
                 
-                print("error!!!"+e)
+                print("error!!!")
                 self.sdRef.close()
 
         print("end")
